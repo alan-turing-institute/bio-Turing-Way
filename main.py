5ead6c95904c40c015a117d7bb1acc4602ab26c3
Iain-S <25081046+Iain-S@users.noreply.github.com>
2021-11-19 16:27:12 +0000

    Copying my_jupyter_book from other repo

---
+++ main.py
@@ -0,0 +1,137 @@
+"""Generate different editions of the book, as determined by profiles.yml."""
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+from shutil import copytree
+from subprocess import run
+
+from yaml import Loader, dump, load
+
+
+def get_toc_and_profiles(book_path):
+    """Get the contents of _toc.yml and profiles.yml."""
+
+    with open(book_path / "_toc.yml") as f:
+        toc = load(f, Loader=Loader)
+
+    with open(book_path / "profiles.yml") as f:
+        profiles = load(f, Loader=Loader)
+
+    return toc, profiles
+
+
+def build(book_path):
+    """Build the book's other editions."""
+
+    toc, profiles = get_toc_and_profiles(book_path)
+
+    for profile_name, new_toc in generate_tocs(toc, profiles):
+        new_path = book_path.parent / (book_path.name + "_" + profile_name)
+        editions_path = book_path / "_build/html/editions"
+
+        # Make the directory if it doesn't already exist
+        editions_path.mkdir(parents=True, exist_ok=True)
+
+        # Copy the whole book
+        copytree(book_path, new_path, dirs_exist_ok=True)
+
+        with open(new_path / "_toc.yml", "w") as f:
+            # Overwrite the _toc.yml
+            dump(new_toc, f)
+
+        # Call Jupyter Book to build the copy
+        run(["jupyter-book", "build", new_path], check=True)
+
+        # Copy the built html to the editions directory
+        copytree(
+            new_path / "_build/html", editions_path / profile_name, dirs_exist_ok=True
+        )
+
+        # ToDo - Delete the new_path after build?
+
+
+def main(args):
+    """Parse arguments and call sub-commands as appropriate."""
+
+    # Get the arguments passed in by the user
+    parser = ArgumentParser(description="Build a Jupyter Book.")
+    subparsers = parser.add_subparsers()
+
+    build_subparser = subparsers.add_parser("build")
+    build_subparser.add_argument(
+        "book_path", type=Path, help="the path to the root of your Jupyter Book"
+    )
+    build_subparser.set_defaults(func=build)
+
+    # Call the sub-parser's function with the other arguments
+    arguments = parser.parse_args(args)
+    arg_dict = vars(arguments)
+    arg_dict.pop("func")(**arg_dict)
+
+
+def mask_parts(components, whitelist):
+    """Makes a new components list, containing only whitelisted files.
+
+    Args:
+        components: An iterable of parts, chapters or sections.
+        whitelist: An iterable of files to keep.
+
+    Returns:
+        A new list of parts, chapters or sections that omits files that aren't
+        whitelisted and components that are now empty.
+    """
+
+    # Don't modify components as they may be needed by other profiles
+    new_components = []
+
+    # We could have a list of parts, chapters or sections
+    for component in components:
+
+        new_component = dict()
+
+        for key, value in component.items():
+            if key == "file":
+                if value in whitelist:
+                    new_component["file"] = value
+
+            elif key in ("parts", "chapters", "sections"):
+                sub_components = mask_parts(value, whitelist)
+                if sub_components:
+                    new_component[key] = sub_components
+
+        if new_component:
+
+            # Add other entries, like "title": "my title"
+            for key, value in component.items():
+                if key not in ("file", "parts", "chapters", "sections"):
+                    new_component[key] = value
+
+            new_components.append(new_component)
+
+    return new_components
+
+
+def mask_toc(toc, whitelist):
+    """Makes a new ToC, containing only whitelisted files.
+
+    Args:
+        toc: A Table of Contents dictionary.
+        whitelist: An iterable of files to keep.
+
+    Returns:
+        A new Table of Contents that omits files that aren't whitelisted and
+        components that are now empty.
+    """
+    masked_toc = mask_parts([toc], whitelist)
+    return masked_toc[0]
+
+
+def generate_tocs(toc, profiles):
+    """Generate a new ToC for each profile."""
+
+    for profile_name, whitelist in profiles.items():
+        yield profile_name, mask_toc(toc, whitelist)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])  # pragma: no cover
---
+++ tests/test_main.py
@@ -0,0 +1,275 @@
+"""Tests for the hello module."""
+import unittest
+from pathlib import Path
+from unittest import mock
+
+from yaml import Loader, load
+
+from main import build, generate_tocs, get_toc_and_profiles, main, mask_parts, mask_toc
+
+
+class TestMain(unittest.TestCase):
+    """Test main function from the main module."""
+
+    def test_main(self):
+        # Redirect stdout
+        with mock.patch("main.build") as mock_build:
+            main(["build", "/path/to/my/book/"])
+
+            mock_build.assert_called_once_with(book_path=Path("/path/to/my/book/"))
+
+
+class TestMask(unittest.TestCase):
+    """Test the mask_x functions from the main module."""
+
+    maxDiff = None
+
+    def test_mask_single_profile(self):
+        whitelist = ["intro", "setup", "version_control/git"]
+
+        with open("tests/test_files/test_one/_toc.yml", "r") as f:
+            toc = load(f, Loader=Loader)
+
+        with open("tests/test_files/test_one/dsg_toc.yml", "r") as f:
+            expected = load(f, Loader=Loader)
+
+        actual = mask_toc(toc, whitelist)
+
+        self.assertDictEqual(expected, actual)
+
+    def test_mask_toc(self):
+        with mock.patch("main.mask_parts") as mock_parts:
+            mock_parts.return_value = [5.5]
+
+            toc = {
+                "format": "jb-book",
+                "root": "intro",
+                "parts": [1, 2, 3],
+            }
+            whitelist = [
+                "a filename",
+            ]
+
+            expected = 5.5
+            actual = mask_toc(toc, whitelist)
+
+            self.assertEqual(expected, actual)
+            mock_parts.assert_called_once_with([toc], whitelist)
+
+    def test_chapters(self):
+        parts = [{"chapters": [{"file": "file1"}, {"file": "file2"}]}]
+        whitelist = [
+            "file1",
+        ]
+
+        expected = [{"chapters": [{"file": "file1"}]}]
+        actual = mask_parts(parts, whitelist)
+        self.assertListEqual(expected, actual)
+
+    def test_sections(self):
+        parts = [
+            {
+                "chapters": [
+                    {"file": "file1", "sections": [{"file": "file2"}]},
+                    {"file": "file3", "sections": [{"file": "file4"}]},
+                ]
+            },
+            {"chapters": [{"file": "file5"}]},
+        ]
+        whitelist = [
+            "file1",
+            "file2",
+        ]
+
+        expected = [
+            {
+                "chapters": [
+                    {"file": "file1", "sections": [{"file": "file2"}]},
+                ]
+            }
+        ]
+        actual = mask_parts(parts, whitelist)
+        self.assertListEqual(expected, actual)
+
+    def test_sub_sections(self):
+        parts = [
+            {
+                "chapters": [
+                    {
+                        "file": "file1",
+                        "sections": [
+                            {
+                                "file": "file2",
+                                "sections": [{"file": "file3"}, {"file": "file4"}],
+                            }
+                        ],
+                    },
+                ]
+            },
+        ]
+        whitelist = [
+            "file1",
+            "file2",
+            "file4",
+        ]
+
+        expected = [
+            {
+                "chapters": [
+                    {
+                        "file": "file1",
+                        "sections": [
+                            {"file": "file2", "sections": [{"file": "file4"}]}
+                        ],
+                    },
+                ]
+            }
+        ]
+        actual = mask_parts(parts, whitelist)
+        self.assertListEqual(expected, actual)
+
+    def test_preserves_title(self):
+        parts = [
+            {
+                "chapters": [
+                    {
+                        "file": "file1",
+                        "title": "title1",
+                        "sections": [
+                            {
+                                "file": "file2",
+                                "title": "title2",
+                                "sections": [
+                                    {"title": "title3", "file": "file3"},
+                                    {"title": "title4", "file": "file4"},
+                                ],
+                            }
+                        ],
+                    },
+                ]
+            },
+        ]
+        whitelist = [
+            "file1",
+            "file2",
+            "file3",
+        ]
+
+        expected = [
+            {
+                "chapters": [
+                    {
+                        "file": "file1",
+                        "title": "title1",
+                        "sections": [
+                            {
+                                "title": "title2",
+                                "file": "file2",
+                                "sections": [{"title": "title3", "file": "file3"}],
+                            }
+                        ],
+                    },
+                ]
+            }
+        ]
+        actual = mask_parts(parts, whitelist)
+        self.assertListEqual(expected, actual)
+
+
+class TestGenerateTocs(unittest.TestCase):
+    """Test the generate_tocs function from the main module."""
+
+    def test_one_profile(self):
+        with mock.patch("main.mask_toc") as mock_mask_toc:
+            mock_mask_toc.return_value = {"new": "toc"}
+
+            toc = {"a": "toc"}
+            profiles = {"dsg": [], "phd": []}
+
+            actual = list(generate_tocs(toc, profiles))
+            expected = [("dsg", {"new": "toc"}), ("phd", {"new": "toc"})]
+            self.assertListEqual(expected, actual)
+
+
+class TestBuild(unittest.TestCase):
+    """Test the build function from the main module."""
+
+    def test_build(self):
+        with mock.patch("main.get_toc_and_profiles") as mock_get:
+            mock_get.return_value = {"a": "toc"}, {"dsg": "profile"}
+
+            with mock.patch("main.generate_tocs") as mock_generate:
+                mock_generate.return_value = [("dsg", {"new": "toc"})]
+
+                with mock.patch("main.copytree") as mock_copy:
+
+                    with mock.patch("main.open") as mock_open:
+
+                        with mock.patch("main.dump") as mock_dump:
+
+                            with mock.patch("main.run") as mock_run:
+
+                                with mock.patch("main.Path.mkdir") as mock_mkdir:
+
+                                    build(Path("mybook"))
+
+                                    mock_mkdir.assert_called_once_with(
+                                        parents=True, exist_ok=True
+                                    )
+
+                                mock_run.assert_called_once_with(
+                                    ["jupyter-book", "build", Path("mybook_dsg")],
+                                    check=True,
+                                )
+
+                            mock_dump.assert_called_once_with(
+                                {"new": "toc"},
+                                mock_open.return_value.__enter__.return_value,
+                            )
+
+                        mock_open.assert_called_with(Path("mybook_dsg/_toc.yml"), "w")
+
+                    mock_copy.assert_has_calls(
+                        [
+                            mock.call(
+                                Path("mybook"), Path("mybook_dsg"), dirs_exist_ok=True
+                            ),
+                            mock.call(
+                                Path("mybook_dsg/_build/html"),
+                                Path("mybook/_build/html/editions/dsg"),
+                                dirs_exist_ok=True,
+                            ),
+                        ]
+                    )
+
+                mock_generate.assert_called_once_with({"a": "toc"}, {"dsg": "profile"})
+
+            mock_get.assert_called_once_with(Path("mybook"))
+
+
+class TestGetTocAndProfiles(unittest.TestCase):
+    """Tet the get_toc_and_profiles function from the main module."""
+
+    def test_simple_case(self):
+        """Check that open() and load() are called."""
+
+        with mock.patch("main.open") as mock_open:
+            with mock.patch("main.load") as mock_load:
+                mock_load.return_value = 44
+
+                path = Path("mybook")
+                toc, profiles = get_toc_and_profiles(path)
+
+                try:
+                    mock_open.assert_any_call(Path("mybook/_toc.yml"))
+                    mock_open.assert_any_call(Path("mybook/profiles.yml"))
+                except AssertionError as e:
+                    print(mock_open.call_args)
+                    raise e
+
+                self.assertEqual(44, toc)
+                self.assertEqual(44, profiles)
+
+
+if __name__ == "__main__":
+    unittest.main()
