07af92e05183a1e42b8ff56a401870f098a91a5f
Yong May Yee <myong@turing.ac.uk>
2021-12-08 16:57:54 +0000

    Output landing page

--- LandingPage.py
+++ LandingPage.py
@@ -1,18 +1,19 @@
 from markdown_it import MarkdownIt
 from mdit_py_plugins.front_matter import front_matter_plugin
 from mdit_py_plugins.footnote import footnote_plugin
+import mdutils as mdutils
 
 class LandingPage:
-  def __init__(self, persona="", page_path=""):
+  def __init__(self, persona=""):
     self.persona = persona
-    self.page_path = page_path
-    self.page_title = "This is a test title"
-  
+    self.curated_page_titles = []
+    self.landing_page_title = "Landing Page:"
+
   def __str__(self):
-    aboutme = "Persona: {0}\n Path: {1}\n Title: {2}".format(self.persona, self.page_path, self.page_title)
+    aboutme = "Persona: {0}\n Path: {1}\n Title: {2}".format(self.persona, self.curated_page_titles, self.landing_page_title)
     return aboutme
   
-  def setPageTitle(self):
+  def getPageTitles(self, curated_page_paths:list):
     def list_header_open(tokens:list)->list:
       header_opens = []
       for t in tokens:
@@ -27,28 +28,50 @@ class LandingPage:
           header_ones.append(t)
       return header_ones
 
-    with open(self.page_path, "r", encoding="utf-8") as input_file:
-      text = input_file.read()
-      md = (MarkdownIt())
-      tokens:list = md.parse(text)
-      # for t in range(0, len(tokens)):
-      #   print("Index: {0}, Type: {1}\n".format(t, tokens[t]))
-      
-      header_opens = list_header_open(tokens)
-      header_ones = list_header_one(header_opens)
-      header_content_index = tokens.index(header_ones[0])+1
-      header_content = tokens[header_content_index].content
-      self.page_title = header_content
-      return self
+    for page_path in curated_page_paths:
+      with open(page_path, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        md = (MarkdownIt())
+        tokens:list = md.parse(text)
+        
+        header_opens = list_header_open(tokens)
+        header_ones = list_header_one(header_opens)
+        header_content_index = tokens.index(header_ones[0])+1
+        header_content = tokens[header_content_index].content
+        self.curated_page_titles.append(header_content)
+    return self
 
-page_path = '/Users/myong/Documents/workspace/bio-Turing-Way/mynewbook/intro.md'    
-aLandingPage=LandingPage("test",page_path)
+  def setPageContent(self):
+    mdFile = mdutils.MdUtils(file_name='./mynewbook/Example_Markdown')
+    mdFile.new_header(level=1, title=self.landing_page_title, add_table_of_contents='n')
+    intro_paragraph = "These are the pages curated for {0}".format(self.persona.upper())
+    mdFile.new_paragraph(intro_paragraph)
+    
+    for title in self.curated_page_titles:
+      mdFile.new_header(level=1, title=title)
 
-aLandingPage = aLandingPage.setPageTitle()
-print(aLandingPage)
+    mdFile.new_table_of_contents(table_title='Curated Pages for {0}'.format(self.persona), depth=2)
+    mdFile.create_md_file()
 
-anotherLandingPage = LandingPage()
-print(anotherLandingPage)
+def sketchMe():
+  curate_page_paths = ['/Users/myong/Documents/workspace/bio-Turing-Way/mynewbook/intro.md']
+  aLandingPage=LandingPage("test")
+  aLandingPage = aLandingPage.getPageTitles(curate_page_paths)
+  aLandingPage.setPageContent()
+  print(aLandingPage)
+
+def getLandingPage(book_path, profiles_and_tocs):
+  print(book_path)
+  for p in range(0,len(profiles_and_tocs)):
+    profile = profiles_and_tocs[p][0]
+    toc = profiles_and_tocs[p][1]
+    print(profile)
+    print(toc['parts'])
+
+
+if __name__ == "__main__":
+  sketchMe()  # pragma: no cover
+
 
 
 # with open(book_path, "r", encoding="utf-8") as input_file:
--- landing.py
+++ landing.py
@@ -3,31 +3,10 @@ import LandingPage as landingPage
 from yaml import Loader, dump, load
 
 
-book_path = '/Users/myong/Documents/workspace/bio-Turing-Way/mynewbook'
+book_path = '/Users/myong/Documents/workspace/bio-Turing-Way/master'
 toc, profiles = services.get_toc_and_profiles(book_path=book_path)
+profiles_and_tocs = list(services.generate_tocs(toc, profiles))
 # print(profiles)
-# print(toc)
+landingPage.getLandingPage(book_path, profiles_and_tocs)
 
-md = (
-    MarkdownIt()
-)
-text = ("""# Welcome to your Jupyter Book
 
-This is a small sample book to give you a feel for how book content is
-structured.
-
-:::{note}
-Here is a note!
-:::
-
-And here is a code block:
-
-```
-e = mc^2
-```
-
-Check out the content pages bundled with this sample book to see more.
-  """)
-
-tokens = md.parse(text)
-print(tokens[0])
