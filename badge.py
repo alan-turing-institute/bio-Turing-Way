51bbcfb5be3c24596a94686e14ac145baff452a0
Iain-S <25081046+Iain-S@users.noreply.github.com>
2021-12-10 15:38:24 +0000

    Remove redundant functions

--- badge.py
+++ badge.py
@@ -3,23 +3,6 @@ import re
 import urllib
 
 
-def extract_recursive(components):
-    files = []
-
-    for component in components:
-        for key, value in component.items():
-            if key == "file":
-                files.append(value)
-            elif key in ("parts", "chapters", "sections"):
-                files = files + extract_recursive(value)
-    return files
-
-
-def extract_files(toc):
-    """Return a list of files in the toc."""
-    return extract_recursive([toc])
-
-
 def generate_badge(profile_name, colour):
     """Return some badge markdown and a list of files to insert it into."""
 
