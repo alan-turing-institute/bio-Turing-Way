608335b46ebca9e6c557c75525c678569c3ce255
Yong May Yee <myong@turing.ac.uk>
2021-12-12 16:17:50 +0000

    Clean away unused code

--- LandingPage.py
+++ LandingPage.py
@@ -1,26 +1,15 @@
-from main import get_toc_and_profiles
+
+import main as services
 from markdown_it import MarkdownIt
-from mdit_py_plugins.front_matter import front_matter_plugin
-from mdit_py_plugins.footnote import footnote_plugin
 import mdutils as mdutils
 import os
-
-class CuratedPage:
-  def __init__(self, page_path, page_title, page_parent):
-    self.page_path=page_path
-    self.page_title=page_title
-    self.page_parent = page_parent
-
-  def __str__(self):
-    aboutme = "Path: {0}\nTitle: {1} Parent:{2}".format(self.page_path, self.page_title,self.page_parent)
-    return aboutme
 
 class LandingPage:
   book_path = ''
   def __init__(self, persona=""):
     self.persona = persona
     self.curated_pages = []
-    self.landing_page_title = "Landing Page:"
+    self.landing_page_title = "Table of Contents for: {0}".format(self.persona)
     self.landing_page_path = os.path.join(self.book_path,self.persona)
     self.mdFile = mdutils.MdUtils(file_name=self.landing_page_path)
 
@@ -55,23 +44,6 @@ class LandingPage:
     curated_links= getLinksOfSection(chapters)
     return curated_links
 
-  def gather_curated_page_paths(self, toc):
-    book = toc['parts'][0]
-    chapters = book['chapters']
-    parent = 1
-    curated_pages = []
-    def getAllFiles(parent, listOfSectionsOrFiles, curated_pages):
-      for section in listOfSectionsOrFiles:
-        if 'file' in section:
-          new_page = {'parent': parent, 'file_path': section['file']+".md"}
-          curated_pages.append(new_page)
-        if 'sections' in section:
-          getAllFiles(parent+1,section['sections'], curated_pages)
-      return curated_pages    
-
-    curated_pages = getAllFiles(parent, chapters, curated_pages)
-    return curated_pages
-
   def get_title_from_curated_page(self, curated_page_path):
     def list_header_open(tokens:list)->list:
       header_opens = []
@@ -97,81 +69,28 @@ class LandingPage:
       header_ones = list_header_one(header_opens)
       header_content_index = tokens.index(header_ones[0])+1
       header_content = tokens[header_content_index].content
-      return header_content
-
-  def populate_from_curated_pages(self, curated_pages:list):
-    """Get curated page titles to be used in generating toc"""
-    def list_header_open(tokens:list)->list:
-      header_opens = []
-      for t in tokens:
-        if (t.type=='heading_open'):
-          header_opens.append(t)
-      return header_opens
-    
-    def list_header_one(header_opens:list):
-      header_ones = []
-      for t in header_opens:
-        if (t.tag=='h1'):
-          header_ones.append(t)
-      return header_ones
-
-    for curated_page in curated_pages:
-      curated_page_path=os.path.join(self.book_path,curated_page['file_path'])
-      curated_page_parent = curated_page['parent']
-      with open(curated_page_path, "r", encoding="utf-8") as input_file:
-        text = input_file.read()
-        md = (MarkdownIt())
-        tokens:list = md.parse(text)
-        
-        header_opens = list_header_open(tokens)
-        header_ones = list_header_one(header_opens)
-        header_content_index = tokens.index(header_ones[0])+1
-        header_content = tokens[header_content_index].content
-        a_curated_page = CuratedPage(page_path=curated_page['file_path'], 
-          page_title=header_content,
-          page_parent=curated_page_parent)
-        self.curated_pages.append(a_curated_page)
-    return self
+    return header_content
 
   def writeContent(self, curated_links):
     """Populate landing page with curated toc"""
-    # new_landing_page_path = os.path.join(self.book_path,self.persona)
-    # mdFile = mdutils.MdUtils(file_name=new_landing_page_path)
-    # mdFile.new_header(level=1, title=self.landing_page_title, add_table_of_contents='n')
+    self.mdFile.new_header(level=1, title=self.landing_page_title, add_table_of_contents='n')
     intro_paragraph = "These are the pages curated for {0}".format(self.persona.upper())
     self.mdFile.new_paragraph(intro_paragraph)
-    
-    # for curated_page in self.curated_pages:
-    #   curated_page:CuratedPage = curated_page
-    #   list_indent:str = ''
-    #   for s in range(0, (curated_page.page_parent-1)*4):
-    #     list_indent:str = list_indent.__add__(" ")
-    #   list_indent = list_indent.__add__("- ")
-    #   link = os.path.join('./',curated_page.page_path)
-    #   md_link=mdFile.new_inline_link(link=link, text=curated_page.page_title)
-    #   mdFile.new_line(list_indent+md_link)
-    #   items = [md_link]
-    self.mdFile.new_list(curated_links)
-      # mdFile.new_line(mdFile.new_inline_link(link=link, text=curated_page.page_title))
-    
+    self.mdFile.new_list(curated_links)   
     self.mdFile.create_md_file()
 
-def sketchMe(curate_page_paths):
-  aLandingPage=LandingPage("test")
-  aLandingPage = aLandingPage.populate_curated_pages(curate_page_paths)
-  print(aLandingPage)
+def setPageContent(book_path, profiles_and_tocs):
+  """Get the each persona and generate a landing page"""
+  LandingPage.book_path=book_path
+  for p in range(0,len(profiles_and_tocs)):
+    profile = profiles_and_tocs[p][0]
+    white_listed_toc = profiles_and_tocs[p][1]
+    aLandingPage = LandingPage(persona=profile)
+    curated_links = aLandingPage.gather_curated_links(white_listed_toc)
+    aLandingPage.writeContent(curated_links)
 
 if __name__ == "__main__":
-  curate_page_paths = ['/Users/myong/Documents/workspace/bio-Turing-Way/mynewbook/intro.md']
-  sketchMe(curate_page_paths)  # pragma: no cover
-
-
-
-# with open(book_path, "r", encoding="utf-8") as input_file:
-#     text = input_file.read()
-# md = (
-#     MarkdownIt()
-# )
-# tokens = md.parse(text)
-# print(type(tokens))
-# print(tokens[1].content)
+  book_path = os.path.join(os.path.dirname(__file__),'master')
+  toc, profiles = services.get_toc_and_profiles(book_path=book_path)
+  profiles_and_tocs = list(services.generate_tocs(toc, profiles))
+  setPageContent(book_path,profiles_and_tocs)
