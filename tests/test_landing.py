2fad23acf933a829c023ec24c0052391519a528c
myyong <myong@turing.ac.uk>
2021-12-13 18:09:25 +0000

    Initial test for landing page

---
+++ tests/test_landing.py
@@ -0,0 +1,32 @@
+import unittest
+from pathlib import Path
+from unittest import mock
+
+from LandingPage import (
+    LandingPage
+)
+
+
+class TestGetPageTitle(unittest.TestCase):
+    """Tests for generate_badge function."""
+
+    def test_simple(self):
+      markdown_text = """
+      # Markdown Files
+      Whether you write your book's content in Jupyter Notebooks (`.ipynb`) or
+      in regular markdown files (`.md`), you'll write in the same flavor of markdown
+      called **MyST Markdown**.
+      """
+      pass
+
+class TestGetCuratedList(unittest.TestCase):
+    """Tests for generate_badge function."""
+
+    def test_simple(self):
+        pass
+
+class TestGetLandingPage(unittest.TestCase):
+    """Tests for generate_badge function."""
+
+    def test_simple(self):
+        pass
